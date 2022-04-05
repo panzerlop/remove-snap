921d8a5ead54469d6915883fa00d28bccadb2f63
panzerlop <94750871+panzerlop@users.noreply.github.com>
2022-04-05 14:34:41 +0100

    Update remove-snap-install-firefox-deb-22.04.sh

--- remove-snap-install-firefox-deb-22.04.sh
+++ remove-snap-install-firefox-deb-22.04.sh
@@ -1,4 +1,34 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# Syntax Derived from script Copyright (C) 2019 madaidan under GPL
+# Snap disable owed to https://github.com/BryanDollery
+# 2022 script panzerlop under GPLv3
+
+set -eu -o pipefail # fail on error and report it, debug all lines
+
+
+script_checks() {
+
+ sudo apt-get update
+ 
+ echo ""
+      if ! ps -p 1 | grep systemd &>/dev/null; then
+      echo "This script can only be used with systemd."
+      exit 1
+    fi
+echo ""
+    if [[ "$(id -u)" -ne 0 ]]; then
+      echo "This script needs to be run as root (sudo)."
+      exit 1
+    fi
+}
+
+
+disable_snap() {
+
+
+  read -r -p "Disable and remove Snap? (y/n) " disable_snap
+	  if [ "${disable_snap}" = "y" ]; then
 
 echo "Removing snap..."
 
@@ -21,9 +51,14 @@ EOF
 sudo mv no-snap.pref /etc/apt/preferences.d/
 sudo chown root:root /etc/apt/preferences.d/no-snap.pref
 
-# Could add a user input choice to install Firefox via Flatpack here...
+    
+  fi
+}
 
-pkill -f firefox
+install_flatpak() {
+
+  read -r -p "InstallFlatpak backend and add flathub? (y/n) " install_flatpak
+	  if [ "${install_flatpak}" = "y" ]; then
 
 sudo add-apt-repository ppa:alexlarsson/flatpak
 
@@ -33,20 +68,79 @@ sudo apt install flatpak -y
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+  fi
+}
+
+install_firefox() {
+
+  read -r -p "InstallFlatpak backend and add flathub? (y/n) " install_firefox
+	  if [ "${install_firefox}" = "y" ]; then
+	  
 flatpak install flathub org.mozilla.firefox
 
-# done
-echo " "
-echo " "
-echo " Snap removed"
-echo " "
-echo " & "
-echo " "
-echo " Firefox Flatpak installed "
-echo " "
 echo " To run Firefox from Terminal type the following: "
 echo " "
 echo " flatpak run org.mozilla.firefox "
 echo " "
 echo " Restart session if Firefox doesn't appear in Applications menu "
 echo " "
+
+  fi
+}
+
+ending() {
+  ## Reboot
+  echo ""
+   echo " https://github.com/panzerlop/"
+   echo ""
+   echo "Hope this helped"
+   echo "Maybe come improve it?"
+    echo ""
+       echo ""
+  read -r -p "Reboot to apply all the changes? (y/n) " reboot
+  if [ "${reboot}" = "y" ]; then
+    reboot
+    
+  fi
+}
+
+echo ""
+echo "22.04 Jelly Jammyfish easy Post-Install guided customisation script"
+echo ""
+cat << "EOF"
+               .-.
+         .-'``(|||)
+      ,`\ \    `-`.
+     /   \ '``-.   `
+   .-.  ,       `___:
+  (:::) :        ___
+   `-`  `       ,   :
+     \   / ,..-`   ,
+      `./ /    .-.`
+         `-..-(   ) 
+               `-`
+EOF
+
+echo ""
+echo ""
+echo "By using this script you accept any break in system function/ damage / blabla is your own fault..."
+echo "So if you also agree with that you may..."
+echo ""
+read -r -p "...start the script? (y/n) " start
+if [ "${start}" = "n" ]; then
+
+echo ""
+
+  exit 1
+elif ! [ "${start}" = "y" ]; then
+  echo ""
+  echo "You did not enter a correct character."
+  echo ""
+  echo "Be careful when reading through this script..."
+  exit 1
+fi
+
+disable_snap
+install_flatpak
+install_firefox
+ending 
